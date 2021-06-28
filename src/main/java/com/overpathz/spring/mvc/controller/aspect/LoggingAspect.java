dbb8c822fd14c204eff7cff8c998b7c4012acb66
overpathz <overpathz@gmail.com>
2021-06-28 13:01:11 +0300

    added AOP

---
+++ src/main/java/com/overpathz/spring/mvc/controller/aspect/LoggingAspect.java
@@ -0,0 +1,24 @@
+package com.overpathz.spring.mvc.controller.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    @Around("execution(* com.overpathz.spring.mvc.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName= methodSignature.getName();
+
+        System.out.println("Begin of " + methodName);
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        System.out.println("End of " + methodName);
+        return targetMethodResult;
+    }
+}
